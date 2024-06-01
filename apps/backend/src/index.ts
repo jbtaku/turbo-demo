15c2ba06ecb712db332802063c2d0bbd630c8662
jbtaku <jbta1086@gmail.com>
2024-06-01 20:31:13 +0900

    fix

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,7 +1,8 @@
 import { Hono } from "hono";
 import { zValidator } from "@hono/zod-validator";
 import { PrismaContext, prismaContext } from "./lib/prisma";
-import { PrefectureCreateInputSchema } from "../prisma/generated/zod";
+import { z } from "zod";
+//import { PrefectureCreateInputSchema } from "../prisma/generated/zod";
 
 const app = new Hono<PrismaContext>()
   .get("/", async (c) => {
@@ -10,17 +11,21 @@ const app = new Hono<PrismaContext>()
     const prefectures = await prisma.prefecture.findMany();
     return c.json(prefectures);
   })
-  .post("/", zValidator("json", PrefectureCreateInputSchema), async (c) => {
-    const prisma = prismaContext(c);
-    const { name, prefectureId } = c.req.valid("json");
-    const prefecture = await prisma.prefecture.create({
-      data: { name, prefectureId },
-    });
-    return c.json(prefecture, 201);
-  });
+  .post(
+    "/",
+    zValidator("json", z.object({ name: z.string(), prefectureId: z.string() })),
+    async (c) => {
+      const prisma = prismaContext(c);
+      const { name, prefectureId } = c.req.valid("json");
+      const prefecture = await prisma.prefecture.create({
+        data: { name, prefectureId },
+      });
+      return c.json(prefecture, 201);
+    }
+  );
 
 export * from "@hono/zod-validator";
 export * from "../prisma/generated/zod";
-export * from "./lib/prisma"
+export * from "./lib/prisma";
 export type AppType = typeof app;
 export default app;
