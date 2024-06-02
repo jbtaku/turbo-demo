41cfc885d5928dbd68e7abe7afb7f68559e2ee4f
jbtaku <jbta1086@gmail.com>
2024-06-02 14:17:02 +0900

    fix

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,17 +1,17 @@
 import React from "react";
 import { client } from "../lib/hono";
-//export const fetchCache = "default-no-store";
+
+export const fetchCache = "default-no-store";
 
 async function Page() {
   /* await client.index.$post({
     json: { name: "bio", prefectureId: "idTestooo" },
   }); */
-  //const res = await fetch("http://localhost:8787/");
   const res = await client.index.$get();
   const data = await res.json();
   return (
     <div>
-      {data.map((item: any) => (
+      {data.map((item) => (
         <p key={item.id}>{item.name}</p>
       ))}
     </div>
