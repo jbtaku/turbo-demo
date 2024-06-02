7cd4de7896c268a066c8ce37e61eb386102bbbad
jbtaku <jbta1086@gmail.com>
2024-06-03 02:14:36 +0900

    fix

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,27 +1,10 @@
 import { Hono } from "hono";
-import { zValidator } from "@hono/zod-validator";
 import { PrismaContext, prismaContext } from "./lib/prisma";
-import { PrefectureCreateInputSchema } from "../prisma/generated/zod";
 
 const app = new Hono<PrismaContext>()
   .get("/", async (c) => {
     const prisma = prismaContext(c);
-
-    const prefectures = await prisma.prefecture.findMany();
-    return c.json(prefectures);
   })
-  .post(
-    "/",
-    zValidator("json", PrefectureCreateInputSchema),
-    async (c) => {
-      const prisma = prismaContext(c);
-      const { name, prefectureId } = c.req.valid("json");
-      const prefecture = await prisma.prefecture.create({
-        data: { name, prefectureId },
-      });
-      return c.json(prefecture, 201);
-    }
-  );
 
 export type AppType = typeof app;
 export default app;
