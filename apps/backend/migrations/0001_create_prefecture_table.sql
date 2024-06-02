fc6c33d3cbff632b8ef61a5f9cb5da58c75d62bd
jbtaku <jbta1086@gmail.com>
2024-06-03 00:11:19 +0900

    add

--- apps/backend/migrations/0001_create_prefecture_table.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Prefecture" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "prefectureId" TEXT NOT NULL,
-    "name" TEXT NOT NULL
-);
-
--- CreateIndex
-CREATE UNIQUE INDEX "Prefecture_prefectureId_key" ON "Prefecture"("prefectureId");
