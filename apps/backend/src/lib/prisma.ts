d3887962cebde3ca5bf1465ca66e0c4cb33abe05
jbtaku <jbta1086@gmail.com>
2024-06-02 12:11:42 +0900

    fix

--- apps/backend/src/lib/prisma.ts
+++ apps/backend/src/lib/prisma.ts
@@ -1,13 +1,13 @@
 import { PrismaClient } from "@prisma/client";
 import { PrismaD1 } from "@prisma/adapter-d1";
 import { Context } from "hono";
+import { D1Database } from "@cloudflare/workers-types";
+export { D1Database } from "@cloudflare/workers-types";
 
 export interface PrismaContext {
   Bindings: { DB: D1Database };
-};
-export const prismaContext = (
-  c: Context<PrismaContext>
-) => {
+}
+export const prismaContext = (c: Context<PrismaContext>) => {
   const adapter = new PrismaD1(c.env.DB);
   const prisma = new PrismaClient({ adapter });
 
