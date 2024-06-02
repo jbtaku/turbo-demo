a9cb0d9a4b68c5db2bc0ffb95a92d27bd6d0dd6b
jbtaku <jbta1086@gmail.com>
2024-06-02 14:01:13 +0900

    fix

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { client } from "../lib/hono";
 
 export const dynamic = "force-dynamic";
-export const fetchCache = "no-store";
+export const fetchCache = "force-no-store";
 
 async function Page() {
   /* await client.index.$post({
