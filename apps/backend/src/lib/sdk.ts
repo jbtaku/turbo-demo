9013f95906208dab8402e66efd740cbd5d449d8a
jbtaku <jbta1086@gmail.com>
2024-06-03 22:16:20 +0900

    fix

--- apps/backend/src/lib/sdk.ts
+++ apps/backend/src/lib/sdk.ts
@@ -3,15 +3,8 @@ import { PrismaD1 } from "@prisma/adapter-d1";
 import { DefaultArgs } from "@prisma/client/runtime/library";
 import { Context } from "hono";
 import Stripe from "stripe";
-import { PrismaClient } from "../../../../node_modules/.prisma/client/index";
+import {PrismaClient} from "@prisma/client";
 
-let prisma: PrismaClient<
-  {
-    adapter: PrismaD1;
-  },
-  never,
-  DefaultArgs
->;
 let stripe: Stripe;
 let isAlreadyInited = false;
 
@@ -19,7 +12,7 @@ const init = new Hono().all("*", async (c: Context, next) => {
   if (!isAlreadyInited) {
     isAlreadyInited = true;
     const adapter = new PrismaD1(c.env.DB);
-    prisma = new PrismaClient({ adapter });
+    const prisma = new PrismaClient({ adapter });
     stripe = new Stripe(c.env.STRIPE_SECRET_KEY, {
       apiVersion: "2024-04-10",
     });
@@ -27,4 +20,4 @@ const init = new Hono().all("*", async (c: Context, next) => {
   await next();
 });
 
-export {init, prisma, stripe}
+export {init, stripe}
