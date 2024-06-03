c70880a86b98c78d61219feb19a276f61458b55f
jbtaku <jbta1086@gmail.com>
2024-06-03 18:23:15 +0900

    fix

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,10 +1,35 @@
 import { Hono } from "hono";
-import { PrismaContext, prismaContext } from "./lib/prisma";
+import { PrismaClient } from "@prisma/client";
+import { PrismaD1 } from "@prisma/adapter-d1";
+import { DefaultArgs } from "@prisma/client/runtime/library";
+import { Context } from "hono";
+//import Stripe from "stripe";
 
-const app = new Hono<PrismaContext>()
-  .get("/", async (c) => {
-    const prisma = prismaContext(c);
-  }).route("/stripe/")
+let prisma: PrismaClient<
+  {
+    adapter: PrismaD1;
+  },
+  never,
+  DefaultArgs
+>;
+//let stripe: Stripe;
+let isAlreadyInited = false;
+
+const app = new Hono()
+  .all("*", async (c: Context, next) => {
+    if (!isAlreadyInited) {
+      isAlreadyInited = true;
+      const adapter = new PrismaD1(c.env.DB);
+      prisma = new PrismaClient({ adapter });
+      /* stripe = new Stripe(c.env.STRIPE_SECRET_KEY, {
+        apiVersion: "2024-04-10",
+      }); */
+    }
+    await next();
+  })
+  //.route("/users", users)
+  //.route("/stripe/checkout", stripeCheckout)
 
 export type AppType = typeof app;
+
 export default app;
